aaa2b2686bd49fa1045a37619f82c828d76d5e09
Achim Friedland <achim.friedland@graphdefined.com>
2026-09-22 15:21:16 +0200

    Linux scripts updated

--- run.sh
+++ run.sh
@@ -6,8 +6,12 @@
 #
 # --help lists the switches.
 
-set -e
+cd libs
+cd Styx;   versionHash_Styx=$(git rev-list --max-count=1 HEAD);   cd ..
+cd Hermod; versionHash_Hermod=$(git rev-list --max-count=1 HEAD); cd ..
+cd ..
 
+set -e
 cd "$(dirname "$0")"
 
-dotnet run --project EVCLI -- "$@"
+dotnet run --no-build --no-restore --project ChargingStationCLI -- $versionHash_Styx $versionHash_Hermod
